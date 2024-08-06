2e7d0d4b2fba3be12089c14c86a95a19ba9ae12e
Marcos Vinicius <marcvinicius.1997@gmail.com>
2024-08-06 14:24:41 -0300

    feat(context): add ConfirmModalProvider and ConfirmModalContext for confirmation modals

---
+++ src/context/ConfirmModalContext.tsx
@@ -0,0 +1,63 @@
+import { createContext, useState, ReactNode } from 'react'
+import { ButtonSmall } from '~/components/ui/Buttons'
+import Modal from '~/components/ConfirmModal'
+
+type ConfirmModalContextProps = {
+  showConfirm: (message: string, onConfirm: () => void) => void
+}
+
+export const ConfirmModalContext = createContext<ConfirmModalContextProps>({
+  showConfirm: () => {},
+})
+
+type ConfirmModalProviderProps = {
+  children: ReactNode
+}
+
+export const ConfirmModalProvider = ({
+  children,
+}: ConfirmModalProviderProps) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [message, setMessage] = useState('')
+  const [onConfirm, setOnConfirm] = useState<() => void>(() => {})
+
+  const showConfirm = (message: string, onConfirm: () => void) => {
+    setMessage(message)
+    setOnConfirm(() => onConfirm)
+    setIsVisible(true)
+  }
+
+  const handleConfirm = () => {
+    onConfirm()
+    setIsVisible(false)
+  }
+
+  const handleCancel = () => {
+    setIsVisible(false)
+  }
+
+  return (
+    <ConfirmModalContext.Provider value={{ showConfirm }}>
+      {children}
+      <Modal
+        show={isVisible}
+        onClose={handleCancel}
+        title='Confirmação'
+        actions={
+          <div className='flex gap-4'>
+            <ButtonSmall onClick={handleCancel}>Não</ButtonSmall>
+            <ButtonSmall
+              bgcolor='#e80537'
+              color='white'
+              onClick={handleConfirm}
+            >
+              Sim
+            </ButtonSmall>
+          </div>
+        }
+      >
+        {message}
+      </Modal>
+    </ConfirmModalContext.Provider>
+  )
+}
