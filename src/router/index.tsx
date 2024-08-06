8aad9976fbf2b532d753b1d2c59cb2f3a9a9db63
Marcos Vinicius <marcvinicius.1997@gmail.com>
2024-08-06 14:15:21 -0300

    refactor(router): replace NewUserPage with NewEmployeePage

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import routes from './routes'
 import DashboardPage from '~/pages/Dashboard'
-import NewUserPage from '~/pages/NewUser'
+import NewEmployeePage from '~/pages/Employee'
 
 const Router = () => {
   return (
@@ -9,7 +9,7 @@ const Router = () => {
       <HashRouter>
         <Routes>
           <Route path={routes.dashboard} element={<DashboardPage />} />
-          <Route path={routes.newUser} element={<NewUserPage />} />
+          <Route path={routes.newUser} element={<NewEmployeePage />} />
           <Route path='*' element={<Navigate to={routes.dashboard} />} />
         </Routes>
       </HashRouter>
